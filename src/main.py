e9e74be3b2d2ce081e0cce1b747bd09134f8d28e
max kolomeychenko <maxim.kolomeychenko@gmail.com>
2021-01-10 19:05:35 +0200

    one tag button for single/miltiselect options

--- src/main.py
+++ src/main.py
@@ -33,47 +33,31 @@ def event_next_image(api: sly.Api, task_id, context, state, app_logger):
     references.refresh_grid(user_id, field)
 
 
-@ag.app.callback("assign_tag_to_object")
+@ag.app.callback("assign_tag")
 @sly.timeit
-def assign_tag_to_object(api: sly.Api, task_id, context, state, app_logger):
+def assign_tag(api: sly.Api, task_id, context, state, app_logger):
     tag_meta = ag.meta.get_tag_meta(ag.reference_tag_name)
     userId = context["userId"]
     figure_id = context["figureId"]
     image_id = context["imageId"]
-    assign(api, figure_id, tag_meta)
 
     image_info = api.image.get_info_by_id(image_id)
     field_value = image_info.meta[ag.field_name]
     ann = cache.get_annotation(image_id)
-    label = ann.get_label_by_id(figure_id)
-    if label is None:
-        raise KeyError(f"Figure with id {figureId} is not found in annotation")
-    references.add(field_value, image_info, label)
-    references.refresh_grid(userId, field_value)
-
-
-@ag.app.callback("multi_assign_tag_to_objects")
-@sly.timeit
-def multi_assign_tag_to_objects(api: sly.Api, task_id, context, state, app_logger):
-    userId = context["userId"]
-    image_id = context["imageId"]
-    image_info = api.image.get_info_by_id(image_id)
-    field_value = image_info.meta[ag.field_name]
-    figure_id = context["figureId"]
-    tag_meta = ag.meta.get_tag_meta(ag.reference_tag_name)
-    ann = cache.get_annotation(image_id)
-
     selected_label = ann.get_label_by_id(figure_id)
+
     if selected_label is None:
         raise KeyError(f"Figure with id {figureId} is not found in annotation")
-    assign(api, figure_id, tag_meta)
-
-    for idx, label in enumerate(ann.labels):
-        if label.geometry.sly_id == figure_id or label.obj_class.name != ag.target_class_name:
-            continue
-        if label.geometry.to_bbox().intersects_with(selected_label.geometry.to_bbox()):
-            assign(api, label.geometry.sly_id, tag_meta)
-            references.add(field_value, image_info, label)
+    if selected_label.obj_class.name == ag.target_class_name:
+        assign(api, figure_id, tag_meta)
+        references.add(field_value, image_info, selected_label)
+    elif selected_label.obj_class.name == ag.multiselect_class_name:
+        for idx, label in enumerate(ann.labels):
+            if label.geometry.sly_id == figure_id:
+                continue
+            if label.geometry.to_bbox().intersects_with(selected_label.geometry.to_bbox()):
+                assign(api, label.geometry.sly_id, tag_meta)
+                references.add(field_value, image_info, label)
     references.refresh_grid(userId, field_value)
 
 
